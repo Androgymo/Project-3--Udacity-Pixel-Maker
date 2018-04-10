bc815406866ffa8855ac44462a46609483561e7b
Androgymo <androgymo@gmail.com>
2018-04-10 14:02:55 -0400

    need to fix the clearing functionality

--- designs.js
+++ designs.js
@@ -1,20 +1,12 @@
 // When size is submitted by the user, call makeGrid()
 
 $(function(){
-	// Select color input
-	$("#colorPicker").on("change",function(){
-		 return $("#colorPicker").val();
-		});
-
-	// Select size input
-
-
+		
 	$("#sizePicker").on("submit", function makeGrid(grid){
 
-		var rows = $("#input_height").val(); // This holds the value of the grid height
-		var columns = $("#input_width").val(); // This holds the value of the grid width
+		var rows = $("#input_height").val(); // # of the grid height
+		var columns = $("#input_width").val(); // # the grid width
 		
-
 
 
 		for(var i = 1; i <= rows; i++){ 
@@ -23,11 +15,12 @@ $(function(){
 				$("tr:last").append("<td></td>");
 				$("td").addClass("cells");
 			}
-			grid.preventDefault();
+			
 			$(".cells").click(function(event){
 				var color = $("#colorPicker").val();
 				$(event.target).css("background-color", color); 
 			})
+			grid.preventDefault();
 		}
 	});
 });
